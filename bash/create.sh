a73782e82c66ed9eddd0a90a50e03a26bdad6fa3
MintyPariogne <dresdenmorgan@hotmail.com>
2016-04-25 18:29:39 -0400

    Update create.sh

--- bash/create.sh
+++ bash/create.sh
@@ -1,2 +1,4 @@
 convert -size 7680×4320 xc:white canvas.jpg
-convert -size 1920x1080 xc:white itunes.jpg
+convert -size 1920x1080 xc:white itunesvideo.jpg
+convert -size 1280×720 xc:white amazonvideo.jpg
+
